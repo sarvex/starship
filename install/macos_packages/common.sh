607eb46423d33fe67a27165a81efc204590f6fb0
Kevin Song <chips@ksong.dev>
2022-02-14 14:55:18 -0600

    Add fallback for failed binary execution

--- install/macos_packages/common.sh
+++ install/macos_packages/common.sh
@@ -11,5 +11,12 @@ starship_version(){
   if [ "$1" = "${1#/}" ]; then
     starship_program_file="./$starship_program_file"
   fi
-  "$starship_program_file" -V | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+  if "$starship_program_file" -V 2>&1 > /dev/null; then
+    "$starship_program_file" -V | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+  else
+    # try to get this information from Cargo.toml
+    pushd "$(git rev-parse --show-toplevel)" || true
+    grep '^version = \"\(.*\)\"' Cargo.toml | cut -f 2 -d '"'
+    popd
+  fi
 }
